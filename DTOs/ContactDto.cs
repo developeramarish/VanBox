4e568ce51d59146227780e5a9837d9fea5ecbd4a
Tanvir Anowar <tanvir.net@gmail.com>
2018-08-18 00:04:46 +0600

    API upgrade Init

--- DTOs/ContactDto.cs
+++
@@ -1,9 +0,0 @@
-namespace VanBox.DTOs
-{
-    public class ContactDto{
-        public string Name { get; set; }
-        public int  Phone { get; set; }
-        public string Email { get; set; }
-
-    }
-}
